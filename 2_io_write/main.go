ec7b111c5354b5ac3e7391c0039627ac3738463b
Kazuki Higashiguchi <k.higashiguchi@binc.jp>
2018-01-27 10:55:14 +0900

    2.2 write file

--- 2_io_write/main.go
+++ 2_io_write/main.go
@@ -1,26 +1,14 @@
 package main
 
 import (
-	"fmt"
+	"os"
 )
 
-// Talker interface
-type Talker interface {
-	Talk()
-}
-
-// Greeter struct
-type Greeter struct {
-	name string
-}
-
-// Talk function
-func (g Greeter) Talk() {
-	fmt.Printf("Hello, my name is %s\n", g.name)
-}
-
 func main() {
-	var talker Talker
-	talker = &Greeter{"wozozo"}
-	talker.Talk()
+	file, err := os.Create("test.txt")
+	if err != nil {
+		panic(err)
+	}
+	file.Write([]byte("os.File example\n"))
+	file.Close()
 }
