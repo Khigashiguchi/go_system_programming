2d49f7d8c0241998f85d297eee8b92817ed6441f
Kazuki Higashiguchi <k.higashiguchi@binc.jp>
2018-04-26 20:03:31 +0900

    3.6.1 use scanner

--- 3_io_read/3.6.1/main.go
+++ 3_io_read/3.6.1/main.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"strings"
 	"fmt"
-	"io"
 )
 
 var source = `一行目
@@ -12,12 +11,16 @@ var source = `一行目
 3行目`
 
 func main() {
-	reader := bufio.NewReader(strings.NewReader(source))
-	for {
-		line, err := reader.ReadString('\n')
-		fmt.Printf("%#v\n", line)
-		if err == io.EOF {
-			break
-		}
+	//reader := bufio.NewReader(strings.NewReader(source))
+	//for {
+	//	line, err := reader.ReadString('\n')
+	//	fmt.Printf("%#v\n", line)
+	//	if err == io.EOF {
+	//		break
+	//	}
+	//}
+	scanner := bufio.NewScanner(strings.NewReader(source))
+	for scanner.Scan() {
+		fmt.Printf("%v\n", scanner.Text())
 	}
 }
