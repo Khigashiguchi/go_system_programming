4cdd7750fa1df6d59100fb38aa5d1d614675dfc1
Kazuki Higashiguchi <k.higashiguchi@binc.jp>
2018-04-26 19:56:59 +0900

    3.5.4 analysis png

--- 3_io_read/3.5.4/analysis-png.go
+++ 3_io_read/3.5.4/analysis-png.go
@@ -3,17 +3,10 @@ package main
 import (
 	"io"
 	"encoding/binary"
-	"fmt"
 	"os"
+	"bytes"
+	"hash/crc32"
 )
-
-func dumpChunk(chunk io.Reader) {
-	var length int32
-	binary.Read(chunk, binary.BigEndian, &length)
-	buffer := make([]byte, 4)
-	chunk.Read(buffer)
-	fmt.Printf("chunk '%v' (%d bytes)\n", string(buffer), length)
-}
 
 func readChunks(file *os.File) []io.Reader {
 	var chunks []io.Reader
@@ -33,14 +26,35 @@ func readChunks(file *os.File) []io.Reader {
 	return chunks
 }
 
+func textChunk(text string) io.Reader {
+	byteData := []byte(text)
+	var buffer bytes.Buffer
+	binary.Write(&buffer, binary.BigEndian, int32(len(byteData)))
+	buffer.WriteString("tEXt")
+	buffer.Write(byteData)
+
+	crc := crc32.NewIEEE()
+	io.WriteString(crc, "tEXt")
+	binary.Write(&buffer, binary.BigEndian, crc.Sum32())
+	return &buffer
+}
+
 func main() {
 	file, err := os.Open("Lenna.png")
 	if err != nil {
 		panic(err)
 	}
 	defer file.Close()
+	newFile, err := os.Create("Lenna2.png")
+	if err != nil {
+		panic(err)
+	}
+	defer newFile.Close()
 	chunks := readChunks(file)
-	for _, chunk := range chunks {
-		dumpChunk(chunk)
+	io.WriteString(newFile, "\x89PNG\r\n\x1a\n")
+	io.Copy(newFile, chunks[0])
+	io.Copy(newFile, textChunk("ASCII PROGRAMMING++"))
+	for _, chunk := range chunks[1:] {
+		io.Copy(newFile, chunk)
 	}
 }
